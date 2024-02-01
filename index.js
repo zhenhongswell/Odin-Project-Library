dbc6a4d82674c89ae2ab9ae8c6fcf0e423fcaa94
zhenhongswell <zhenhongswell@gmail.com>
2024-02-01 13:53:39 +0800

    finish the Library functionality
    -display books on the page
    -add book to mylibrary after pressing the submit button
    -add a button on each book's display to remove itself
    -add a button on each book's display to change it's read status
    
    would add the storage later on in the course

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const myLibrary = [];
+let myLibrary = [];
 
-const Book = function(name,page){
+const dialog = document.querySelector("dialog");
+
+const bookNameInput = document.querySelector('.nameInput');
+
+const content = document.querySelector(".content");
+
+const newBookBtn = document.querySelector('.createNewBook');
+
+const closeButton = document.querySelector("dialog .close");
+
+const submitButton = document.querySelector('dialog .sumbit');
+
+
+const Book = function(name,read){
     this.name = name;
-    this.page = page;
+    this.read = read;
 }
 
-// let Book_Object = {
-//    name: 'default-book-name',
-// }
-
-// let newbook_object = Object.create(Book_Object);
-// newbook_object.name = 'test';
-
-function addBookToLibrary(name,page){
-    let newbook = new Book(name,page);
+function addBookToLibrary(newbook){
     myLibrary.push(newbook);
 }
 
@@ -23,11 +28,87 @@ function displayMyLibrary(){
     });
 }
 
+newBookBtn.addEventListener("click",(event)=>{
+    dialog.showModal();
+});
 
-addBookToLibrary('test1','12');
-addBookToLibrary('test2','15');
-addBookToLibrary('test3','20');
-displayMyLibrary();
+closeButton.addEventListener("click", () => {
+    if (bookNameInput.value === '') {
+        dialog.close();
+        return;
+    }
+    bookNameInput.value = '';
+    dialog.close();
+});
+
+function myLibrary_remove_by_book_name(arr,value){
+    return arr.filter(function(ele){
+        return ele.name != value;
+    });
+}
+
+submitButton.addEventListener("click", () => {
+    if (bookNameInput.value === '') {
+        dialog.close();
+        return;
+    }
+    const book_div = document.createElement('div');
+
+    let newbook = new Book(bookNameInput.value,false);
+    addBookToLibrary(newbook);
+    book_div.classList.add('book');
+    const book_name = document.createElement('div');
+    book_name.textContent = `book name: ${newbook.name}`;
+    book_div.appendChild(book_name);
+    book_div.setAttribute('data',newbook.name);
+
+    
+    const book_read_lable = document.createElement('label');
+    book_read_lable.textContent = 'read';
+    book_div.appendChild(book_read_lable);
+
+    const book_read = document.createElement('input');
+    book_read.setAttribute('id','checkbox');
+    book_read.setAttribute('type','checkbox');
+    book_read.setAttribute('data',newbook.name);
+    book_read.addEventListener('click',(e)=>{
+        let bookReadArray = myLibrary.map(element =>{
+            // return element;
+            if (element.name === book_read.getAttribute('data')){
+                element.read = book_read.checked;
+                return element;
+            }
+            else{
+                return element;
+            }
+        })
+        myLibrary = bookReadArray.slice();
+        displayMyLibrary();
+    })
+
+    book_div.appendChild(book_read);
+    
+    // adding the remove button
+    const removeBookBtn = document.createElement('button');
+    removeBookBtn.textContent = 'remove This Book!';
+    removeBookBtn.addEventListener('click',function(e){
+        const bookNameToRemove = this.parentElement.getAttribute('data');
+        const filtered_array = myLibrary_remove_by_book_name(myLibrary,bookNameToRemove);
+        myLibrary = filtered_array.slice();
+        // displayMyLibrary();
+        this.parentElement.remove();
+    })
+
+    book_div.appendChild(removeBookBtn);
+
+    content.appendChild(book_div);
+
+
+    bookNameInput.value = '';
+    dialog.close();
+});
+
+
 
 
 
