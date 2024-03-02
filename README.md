903ef17b5064a4cbe6851b3e1eb79e0dbcc365d7
zhenhongswell <zhenhongswell@gmail.com>
2024-03-02 19:47:12 +0800

    refactor to class -Library to store book -create book then push to Library

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Odin-Project-Library
 Odin-Project:Library
+
+store the library as an array
+>initialize library array as empty
+>push the book ojbect to array ,making 1d array
+
+store the book as class
+-variable name
+-variable page
+-variable read:true/false
+
+add button to create book then push to library
+
--- index.js
+++ index.js
@@ -9,3 +9,38 @@ const newBookBtn = document.querySelector('.createNewBook');
 const closeButton = document.querySelector("dialog .close");
 
 const submitButton = document.querySelector('dialog .sumbit');
+
+
+class Library{
+    // empty array
+    constructor(){
+        this.storage = [] 
+    }
+
+    addBook(name){
+        const newBook = new Book(name)
+        this.storage.push(newBook);
+    }
+
+    showBook(){
+        console.log(this.storage)
+    }
+}
+
+class Book{
+    constructor(name){
+        this.name = name
+    }
+
+    getBook(){
+        return this
+    }
+}
+
+
+const newLibrary = new Library()
+newLibrary.addBook("1")
+
+
+
+newLibrary.showBook()
